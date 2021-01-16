9a521bb9c8c2cb7ba71341ea6e43d0fdd80ffe7e
zzrlc <2467254599@qq.com>
2021-01-16 16:27:19 +0800

    添加备用url

--- auto-spidercomic.py
+++ auto-spidercomic.py
@@ -4,32 +4,35 @@ Created on 20201225
 
 @author: ZZR
 """
+import logging
 import sys
-import threading
+import time
+from ImageCrawlProcess import ImageCrawl as IC
 
-from ImageCrawl import ImageCrawl as IC
-
-thread_max_num = threading.Semaphore(10)
 
 def downloadComic(url):
+    logging.info('==========正在Spider url=',url,'的网页==========')
     print('==========正在Spider url=',url,'的网页==========')
     craw = IC()
     craw.getImage(url)
 
 if __name__=="__main__":
-    # 读取comics.txt中的urls
+    time_start=time.time()
+
     urls=[]
     with open('.\comicsTxts\comics.txt','r') as f:
         for line in f:
             urls.append(line.strip('\n'))
-        print(urls)
-    # 循环spider url
-    # with thread_max_num:
-    for url in urls:
-        # 单线程下载
-        # downloadComic(url)
 
-        # 开启多线程进行下载
-        th = threading.Thread(target = downloadComic, args=(url,))
-        th.start()
+    urls = [x for x in urls if x != '']
+    print(urls)
+
+    if len(urls):
+        sys.exit()
+
+    for url in urls:
+        downloadComic(url)
+    
+    time_end=time.time()
+    print('程序耗时：', time_end-time_start ,'秒')
 
--- test.py
+++ test.py
@@ -19,8 +19,8 @@ from requests.adapters import HTTPAdapter
 ress = requests.Session()
 
 # max_retries 为最大重试次数，重试3次，加上最初的一次请求，一共是4次，所以上述代码运行耗时是20秒而不是15秒
-ress.mount('http://', HTTPAdapter(max_retries=2))
-ress.mount('https://', HTTPAdapter(max_retries=2))
+ress.mount('http://', HTTPAdapter(max_retries=3))
+ress.mount('https://', HTTPAdapter(max_retries=3))
 
 headers={"User-Agent" : "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6) ",
   "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -32,31 +32,31 @@ proxies = {'http': 'http://localhost:4780', 'https': 'http://localhost:4780'}
 
 #定义一个爬取图片的类
 class ImageCrawl(object):
+    def setPic(picname,r):
+        with open(picname,"wb") as f:
+            for image in r.iter_content(chunk_size=1024):
+                if image:
+                    f.write(image)
+                    f.flush
+
     def download(self,url,filename,picname,backUpUrl,backUpPicname):
         if  not os.path.exists(filename):
             os.makedirs(filename)
         
         if os.path.exists(picname):
-            # print('该文件已下载')
+            print('该文件已下载')
             pass
         else:
             try:
                 r = ress.get(url,stream=True,timeout=5,proxies=proxies)
-                # 每访问一次，休眠几秒
-                randomTime = random.randint(2,3)
-                time.sleep(randomTime)
                 if r.status_code == 404:
-                    print('图片不存在，启用备用url')
-                    backUpUrlFilestr = backUpUrl.split('.')[-1]
-                    if backUpUrlFilestr == 'svg':
-                        pass
-                    else:
-                        rb = ress.get(backUpUrl,stream=True,timeout=5,proxies=proxies)
-                        with open(backUpPicname,"wb") as f:
-                            for image in rb.iter_content(chunk_size=1024):
-                                if image:
-                                    f.write(image)
-                                    f.flush
+                    print('is 404')
+                    rb = ress.get(backUpUrl,stream=True,timeout=5,proxies=proxies)
+                    with open(backUpPicname,"wb") as f:
+                        for image in rb.iter_content(chunk_size=1024):
+                            if image:
+                                f.write(image)
+                                f.flush
                 else:
                     with open(picname,"wb") as f:
                         for image in r.iter_content(chunk_size=1024):
@@ -65,13 +65,6 @@ class ImageCrawl(object):
                                 f.flush
             except requests.exceptions.RequestException as e:
                 print(e)
-                r = ress.get(backUpUrl,stream=True,timeout=5,proxies=proxies)
-               
-                with open(picname,"wb") as f:
-                    for image in r.iter_content(chunk_size=1024):
-                        if image:
-                            f.write(image)
-                            f.flush
 
                         
     def getImage(self, url):
@@ -94,40 +87,46 @@ class ImageCrawl(object):
             randomTime = random.randint(1,3)
             fileName = time.strftime('%Y-%m-%d-%H:%M:%S',time.localtime(time.time())) + '-' + str(randomTime)
 
+        print('文件夹名：', fileName)
 
         allLen = len(allImg) / 2
-        print('文件夹名：', fileName, '共：', allLen, '张')
 
         print('Parent process %s.' % os.getpid())
         p = Pool(multiprocessing.cpu_count())
 
-        for img in allImg:
-            # print(img.attrs['onerror'])
-            attrs = img.attrs
+        img = allImg[0]
 
-            dataSrc = attrs['src']
+        print('img', img)
 
-            backUpUrl = attrs['onerror']
-            backUpUrl = backUpUrl.replace("javascript:this.src='", " ", 1)
-            backUpUrl = backUpUrl.replace("';this.onerror = null", " ", 1)
-            backUpUrl = backUpUrl.strip()
+        attrs = img.attrs
+        dataSrc = attrs['onerror']
+        dataSrc = dataSrc.replace("javascript:this.src='", " ", 1)
+        dataSrc = dataSrc.replace("';this.onerror = null", " ", 1)
+        dataSrc = dataSrc.strip()
 
-            # print('dataSrc:', dataSrc)
-            # print('backUpUrl:', backUpUrl)
-            # 获取文件后缀，如果是svg则不下载
-            filestr = dataSrc.split('.')[-1]
-            if filestr == 'svg':
-                pass
-            else:
-                picname = os.path.join(fileName, dataSrc.split('/')[-1])
-                # print('图片名称：', picname, '，共：',allLen,'张')
-                backUpPicname = os.path.join(fileName, backUpUrl.split('/')[-1])
-                # print('备用图片名：', backUpPicname)
-                p.apply_async(self.download, args=(dataSrc,fileName,picname,backUpUrl,backUpPicname))
+        backUpUrl = attrs['data-src']
+        #   'https://i0.nyacdn.com/galleries/1110228/1.jpg'
+        print('dataSrc:', dataSrc)
+        print('backUpUrl:', backUpUrl)
+        # 获取文件后缀，如果是svg则不下载
+        filestr = dataSrc.split('.')[-1]
+        if filestr == 'svg':
+            pass
+        else:
+            picname = os.path.join(fileName, dataSrc.split('/')[-1])
+            print('图片名称：', picname, '，共：',allLen,'张')
+
+            backUpPicname = os.path.join(fileName, backUpUrl.split('/')[-1])
+            print('备用图片名：', backUpPicname, '，共：',allLen,'张')
+            p.apply_async(self.download, args=(dataSrc,fileName,picname,backUpUrl,backUpPicname))
 
         print('等待所有子进程结束...')
         p.close()
         p.join()
         print('所有子进程结束。')
 
-        
+if __name__=="__main__":
+    craw = ImageCrawl()
+    craw.getImage('https://zhb.doghentai.com/g/207252/list2/')
+    # r = ress.get('https://i0.nyacdn.com/galleries/1110228/1.jpg',stream=True,timeout=5,proxies=proxies)
+    # print(r.status_code)
